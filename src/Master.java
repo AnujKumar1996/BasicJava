cecf086aef3b0cedd30ea070f1633cf7c16b1159
Anuj Kumar <AnKumar@ESTUATE.COM>
2022-09-26 11:07:14 +0530

    commiting

---
+++ src/Master.java
@@ -0,0 +1,9 @@
+
+public class Master {
+void biggestElementBetweenThree(int num1,int num2, int num3) {
+	if(num1>=num2&&num1>=num3) {
+		System.out.println(num1+" is greatest number");
+	}
+}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int num1=sc.nextInt();
+	int num2=sc.nextInt();
+	int num3=sc.nextInt();
+	Master m=new Master();
+	m.biggestElementBetweenThree(num3, num3, num3);
+}
+}
