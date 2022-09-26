733b630bf9f16c9e0d8c9e7d287aa6c840a24d9a
Anuj Kumar <AnKumar@ESTUATE.COM>
2022-09-26 14:26:14 +0530

    added

--- src/Master.java
+++ src/Master.java
@@ -1,16 +1,37 @@
 
 public class Master {
-void biggestElementBetweenThree(int num1,int num2, int num3) {
-	if(num1>=num2&&num1>=num3) {
-		System.out.println(num1+" is greatest number");
+	void allMathematic(int num1, int num2, int num3) {
+		int result;
+		System.out.println( result=num1+num2+num3);
 	}
-	else if(num2>=num1&&num2>=num3) {
-		System.out.println(num2+" is greatest number");
+	void biggestElementBetweenThree(int num1, int num2, int num3) {
+		if (num1 >= num2 && num1 >= num3) {
+			System.out.println(num1 + " is greatest number");
+		} else if (num2 >= num1 && num2 >= num3) {
+			System.out.println(num2 + " is greatest number");
+		} else {
+			System.out.println(num3 + " is greatest number");
+		}
 	}
-	else
-	{
-		System.out.println(num3+" is greatest number");
+
+	void evenOdd(int num1) {
+		if (num1 % 2 == 0) {
+			System.out.println(num1 + " is a Even Number");
+		} else
+			System.out.println(num1 + " is a odd number");
 	}
-}
+
+	void printTable(int num1) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(num1 + " * " + i + " = " + num1 * i);
+		}
+	}
+
+	void average(int num1, int num2, int num3) {
+		int result;
+		result = (num1 + num2 + num3) / 3;
+		System.out.println("Average = " + result);
+		System.out.println();
+	}
 
 }
--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,58 @@
 import java.util.Scanner;
 
 public class Test {
-public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter three number");
-	int num1=sc.nextInt();
-	int num2=sc.nextInt();
-	int num3=sc.nextInt();
-	Master m=new Master();
-	m.biggestElementBetweenThree(num1, num2, num3);
-}
+	public static void main(String[] args) {
+		Master m = new Master();
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1. Biggest Element Between Three Element");
+		System.out.println("2. Find Even & Odd Between two Numbers");
+		System.out.println("3. Print table ");
+		System.out.println("4. Find average ");
+		System.out.println("Select The operation number you want to Perform");
+		System.err.println("----------------------------------");
+		int op = sc.nextInt();
+//		All Mathematics OPerations
+		 if (op == 1) {
+			System.out.println("Enter the three integer numbers");
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			int num3 = sc.nextInt();
+			m.average(num1, num2, num3);
+		}
+		
+//		Biggest Elemet
+		 else if (op == 2) {
+			System.out.println("Enter the three integer numbers");
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			int num3 = sc.nextInt();
+			m.biggestElementBetweenThree(num1, num2, num3);
+//			Even Odd number
+		}  
+		else if (op == 3) {
+			System.out.println("Enter a integer numbers");
+			int num1 = sc.nextInt();
+			m.evenOdd(num1);
+		} 
+//		Print table
+		else if (op == 4) {
+			System.out.println("Enter a Number");
+			int num1 = sc.nextInt();
+			m.printTable(num1);
+		}
+//		Find Average
+		else if (op == 5) {
+			System.out.println("Enter the three integer numbers");
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			int num3 = sc.nextInt();
+			m.average(num1, num2, num3);
+		}
+
+		else {
+			System.err.println("Ïnvalid Please enter a Valid Opetions !!");
+		}
+
+	}
 }
