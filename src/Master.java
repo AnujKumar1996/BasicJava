726aee3294f24b9b3f4cee41db3c55edd0e2dd43
Anuj Kumar <AnKumar@ESTUATE.COM>
2022-09-26 11:49:48 +0530

    added

--- src/Master.java
+++ src/Master.java
@@ -4,6 +4,13 @@ void biggestElementBetweenThree(int num1,int num2, int num3) {
 	if(num1>=num2&&num1>=num3) {
 		System.out.println(num1+" is greatest number");
 	}
+	else if(num2>=num1&&num2>=num3) {
+		System.out.println(num2+" is greatest number");
+	}
+	else
+	{
+		System.out.println(num3+" is greatest number");
+	}
 }
 
 }
--- src/Test.java
+++ src/Test.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Test {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter three number");
 	int num1=sc.nextInt();
 	int num2=sc.nextInt();
 	int num3=sc.nextInt();
 	Master m=new Master();
-	m.biggestElementBetweenThree(num3, num3, num3);
+	m.biggestElementBetweenThree(num1, num2, num3);
 }
 }
