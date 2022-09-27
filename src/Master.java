66b52d021456139acb52e232eb66e239ba5bb3db
Anuj Kumar <AnKumar@ESTUATE.COM>
2022-09-27 09:31:45 +0530

    ad

--- src/Master.java
+++ src/Master.java
@@ -1,11 +1,20 @@
 
 public class Master {
-	void allMathematic(int num1, int num2, int num3) {
-		int result;
-		result=num1+num2+num3;
-		
-		System.out.println();
+//	1. perform all operation  add, subtraction, multiplication division
+	void allMathematic(int num1, int num2) {
+		int add, sub, mul;
+		add = num1 + num2;
+		sub = num1 - num2;
+		mul = num1 * num2;
+		float div = (float) num1 / num2;
+		float r = num1 % num2;
+		System.out.println("Add = " + add);
+		System.out.println("Subtraction =  " + sub);
+		System.out.println("Mul = " + mul);
+		System.out.println("Division = " + div + "\n And reminder = " + r);
 	}
+
+//2. find biggest between three number
 	void biggestElementBetweenThree(int num1, int num2, int num3) {
 		if (num1 >= num2 && num1 >= num3) {
 			System.out.println(num1 + " is greatest number");
@@ -16,24 +25,142 @@ public class Master {
 		}
 	}
 
+//3. find even and off from two number
 	void evenOdd(int num1) {
 		if (num1 % 2 == 0) {
 			System.out.println(num1 + " is a Even Number");
 		} else
-			System.out.println(num1 + " is a odd number");
+			System.err.println(num1 + " is a odd number");
 	}
 
+//4 print the table for a given number
 	void printTable(int num1) {
 		for (int i = 1; i <= 10; i++) {
 			System.out.println(num1 + " * " + i + " = " + num1 * i);
 		}
 	}
-//average
+
+//5. average
 	void average(int num1, int num2, int num3) {
-		int result;
-		result = (num1 + num2 + num3) / 3;
+		float result;
+		result = (float) (num1 + num2 + num3) / 3;
 		System.out.println("Average = " + result);
 		System.out.println();
 	}
 
+//	6. sum of all number up to a range
+	void SumOfNumber(int num1) {
+		long sum = 0;
+		for (int i = 1; i <= num1; i++) {
+			sum = (long) sum + i;
+		}
+		System.out.println("Sum of all the number between is = " + sum);
+	}
+
+//	7. prime number
+	void primeNumber(int num1) {
+		int count = 0;
+		for (int i = 2; i <= num1; i++) {
+			if (num1 % i == 0) {
+				count++;
+			}
+		}
+		if (count > 1) {
+			System.err.println(num1 + " is NOT a Prime number");
+		} else {
+			System.out.println(num1 + " is a prime number");
+		}
+	}
+
+// 8.	Factorial 
+	void factorial(int num1) {
+		long fact = 1;
+		for (int i = 1; i <= num1; i++) {
+			fact = fact * i;
+		}
+		System.out.println("Factorial of " + num1 + " = " + fact);
+	}
+
+//9. Reverse Number
+	void reverseNumber(int num1) {
+		long rev = 0;
+		while (num1 != 0) {
+			int r = num1 % 10;
+			rev = rev * 10 + r;
+			num1 = num1 / 10;
+		}
+		System.out.println("Reverse of number = " + rev);
+	}
+
+//10. Palindrome Number
+	void palindromeNumber(int num1) {
+		long rev = 0;
+		int temp = num1;
+		while (num1 != 0) {
+			int r = num1 % 10;
+			rev = rev * 10 + r;
+			num1 = num1 / 10;
+		}
+		if (rev == temp)
+			System.out.println(temp + " is a Palindrome number");
+		else
+			System.err.println(temp + " is not a plindrome Number");
+	}
+
+// 11. A simple calculator
+	void calculator(int num1, int num2, int num3) {
+		int result;
+		switch (num3) {
+
+		case '+':
+			result = num1 + num2;
+			System.out.println(num1 + " + " + num2 + " = " + result);
+			break;
+
+		case '-':
+			result = num1 - num2;
+			System.out.println(num1 + " - " + num2 + " = " + result);
+			break;
+
+		case '*':
+			result = num1 * num2;
+			System.out.println(num1 + " * " + num2 + " = " + result);
+			break;
+
+		case '/':
+			float div = (float) num1 / num2;
+			System.out.println(num1 + " / " + num2 + " = " + div);
+			break;
+
+		default:
+			System.out.println("Invalid operator!");
+			break;
+		}
+
+	}
+
+//	12. Armstrong Number
+	void armstrongNumber(int num1) {
+		int r;
+		double arm = 0;
+		int temp = num1;
+		int count = 0;
+		int n = num1;
+		while (num1 != 0) {
+			int rm = num1 / 10;
+			count++;
+			num1 = num1 / 10;
+		}
+
+		while (n != 0) {
+			r = n % 10;
+			arm = arm + (Math.pow(r, count));
+			n = n / 10;
+		}
+		if (temp == arm) {
+			System.out.println(temp + " is a armstrong number");
+		} else {
+			System.err.println(temp + " is not a armstrong number");
+		}
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -2,44 +2,53 @@ import java.util.Scanner;
 
 public class Test {
 	public static void main(String[] args) {
-		Master m = new Master();
+		Master obj = new Master();
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("1. Biggest Element Between Three Element");
-		System.out.println("2. Find Even & Odd Between two Numbers");
-		System.out.println("3. Print table ");
-		System.out.println("4. Find average ");
+		System.out.println("1.Perform all Operation like ,+ ,- , * , / ");
+		System.out.println("2. Biggest Element Between Three Element");
+		System.out.println("3. Find Even & Odd Between two Numbers");
+		System.out.println("4. Print table ");
+		System.out.println("5. Find average ");
+		System.out.println("6. Find Sum of all Number upto a range ");
+		System.out.println("7. Find Prime Number ");
+		System.out.println("8. Find Factorial");
+		System.out.println("9. Find Reverse Number");
+		System.out.println("10. Find Palindrome Number");
+		System.out.println("11. A simple calculator ");
+		System.out.println("12. Find Armstrong Number ");
+
+		System.out.println("_____________________________________________________");
 		System.out.println("Select The operation number you want to Perform");
-		System.err.println("----------------------------------");
 		int op = sc.nextInt();
+		System.err.println("-----------------------------------------------");
+
 //		All Mathematics OPerations
-		 if (op == 1) {
-			System.out.println("Enter the three integer numbers");
+		if (op == 1) {
+			System.out.println("Enter two integer numbers");
 			int num1 = sc.nextInt();
 			int num2 = sc.nextInt();
-			int num3 = sc.nextInt();
-			m.average(num1, num2, num3);
+			obj.allMathematic(num1, num2);
 		}
-		
-//		Biggest Elemet
-		 else if (op == 2) {
+
+//		Biggest Element
+		else if (op == 2) {
 			System.out.println("Enter the three integer numbers");
 			int num1 = sc.nextInt();
 			int num2 = sc.nextInt();
 			int num3 = sc.nextInt();
-			m.biggestElementBetweenThree(num1, num2, num3);
+			obj.biggestElementBetweenThree(num1, num2, num3);
 //			Even Odd number
-		}  
-		else if (op == 3) {
+		} else if (op == 3) {
 			System.out.println("Enter a integer numbers");
 			int num1 = sc.nextInt();
-			m.evenOdd(num1);
-		} 
+			obj.evenOdd(num1);
+		}
 //		Print table
 		else if (op == 4) {
-			System.out.println("Enter a Number");
+			System.out.println("Enter a Integer Number");
 			int num1 = sc.nextInt();
-			m.printTable(num1);
+			obj.printTable(num1);
 		}
 //		Find Average
 		else if (op == 5) {
@@ -47,12 +56,55 @@ public class Test {
 			int num1 = sc.nextInt();
 			int num2 = sc.nextInt();
 			int num3 = sc.nextInt();
-			m.average(num1, num2, num3);
+			obj.average(num1, num2, num3);
+		}
+//		 Sum of all number up to a range
+		else if (op == 6) {
+			System.out.println("Enter a Integer Number");
+			int num1 = sc.nextInt();
+			obj.SumOfNumber(num1);
+		}
+//		 Find prime number
+		else if (op == 7) {
+			System.out.println("Enter a Integer Number");
+			int num1 = sc.nextInt();
+			obj.primeNumber(num1);
+		}
+//		 Find Factorial of a number
+		else if (op == 8) {
+			System.out.println("Enter a Integer Number");
+			int num1 = sc.nextInt();
+			obj.factorial(num1);
+		} else if (op == 9) {
+			System.out.println("Enter a Integer Number");
+			int num1 = sc.nextInt();
+			obj.reverseNumber(num1);
+		}
+//		Palindrome Number
+		else if (op == 10) {
+			System.out.println("Enter a Integer Number");
+			int num1 = sc.nextInt();
+			obj.palindromeNumber(num1);
+		}
+//		calculator 
+		else if (op == 11) {
+			System.out.println("Choose an operator: +, -, *, or /");
+			int num3 = sc.next().charAt(0);
+
+			System.out.println("Enter first number");
+			int num1 = sc.nextInt();
+
+			System.out.println("Enter second number");
+			int num2 = sc.nextInt();
+			obj.calculator(num1, num2, num3);
+		} else if (op == 12) {
+			System.out.println("Enter a Integer Number");
+			int num1 = sc.nextInt();
+			obj.armstrongNumber(num1);
 		}
 
-		else {
+		else
 			System.err.println("Ïnvalid Please enter a Valid Opetions !!");
-		}
-
 	}
+
 }
